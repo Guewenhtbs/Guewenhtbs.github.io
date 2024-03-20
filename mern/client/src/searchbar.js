6ea7e8fc5a4ebadc979d36d8a1efc74c5647725c
Guewenhtbs <kerguegue@gmail.com>
2024-03-20 10:54:16 +0100

    (barre_recherche): barre de recherhce avec boutons de selection

--- mern/client/src/searchbar.js
+++ mern/client/src/searchbar.js
@@ -1,64 +1,74 @@
-const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-  
-    // Fonction pour mettre à jour les résultats de recherche
-    const handleSearch = (query) => {
-      setSearchQuery(query);
-  
-      if (query === '') {
-        setSearchResults([]);
-      }
-      else {
-        // Simuler la recherche en filtrant une liste de résultats
-        const filteredResults = liste_prof.filter(item =>
-          item.toLowerCase().includes(query.toLowerCase())
-        );
-        setSearchResults(filteredResults);
-      }
-  
-      
-    };
-  
-    return (
-      <div>
-        <input 
-          className="bar"
-          type="text"
-          placeholder="Rechercher..."
-          value={searchQuery}
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-        
-        <div className="results">
-          {searchResults.map((result, index) => (
-          <button key={index} className="result-item">
-            {result}
-          </button>
-          ))}
-        </div>
-      </div>
-    );
-  };
-  
-  
+import React, { useState } from 'react';
+import './searchbar.css';
+
+function SearchBar() {
   // Données de test
-  const liste_prof = [
+  const [listProf, setlistProf] = useState([
     'Stéphane Frenot',
     'Pierre Francois',
     'Oana Iova',
     'Victor Rebecq',
     'Chantal Muller',
     'Claire Goursaud',
-  ];
-  
-  const App = () => {
-    return (
-      <div>
-        <h1>Trouve ton prof préféré</h1>
-        <SearchBar />
-      </div>
-    );
+  ]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Fonction pour mettre à jour les résultats de recherche
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+
+    if (query === '') {
+      setSearchResults([]);
+    }
+    else {
+      // Simuler la recherche en filtrant une liste de résultats
+      const filteredResults = listProf.filter(item =>
+        item.toLowerCase().includes(query.toLowerCase())
+      );
+      setSearchResults(filteredResults);
+    }
   };
   
-  export default App;
+
+  function handleClick(text) {
+    alert(text);
+    // Remplacer alert par la fonction de requête de personne
+    setSearchQuery('');
+    setSearchResults([]);
+    setlistProf(listProf.filter(item => item!== text));
+  }
+
+  return (
+    <div>
+      <input 
+        className="bar"
+        type="text"
+        placeholder="Rechercher..."
+        value={searchQuery}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      
+      <div className="results">
+        {searchResults.map((result, index) => (
+        <button key={index} className='result-item' onClick={() => handleClick(result)}>{result}</button>
+        ))}
+      </div>
+    </div>
+  );
+
+};
+
+  
+
+  
+const App = () => {
+    return (
+        <div>
+        <h1>Trouve ton prof préféré</h1>
+        <SearchBar />
+        </div>
+    );
+};
+
+export default App;
