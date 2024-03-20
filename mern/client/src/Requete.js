fe7f4c3be3e67cd700a5e8f968d0227eaba4aaa9
eoliennne <likavf3@gmail.com>
2024-03-20 10:49:19 +0100

    fonction fetch post

---
+++ mern/client/src/Requete.js
@@ -0,0 +1,19 @@
+const requeteClient = async (code) => {
+    try {
+      const response = await fetch('https://fetchurl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: code
+      });
+
+      if (!response.ok) {
+        throw new Error('Erreur réseau');
+      }
+
+      console.log('Envoi sur l API réussi');
+    } catch (error) {
+      console.error('Il y a eu une erreur lors du fetch:', error);
+    }
+  };
