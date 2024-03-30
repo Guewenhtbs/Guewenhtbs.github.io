ea135dc03f4885e2fd5520620cd7cc0ed855efd3
Allisson22 <allisson.roussel@insa-lyon.fr>
2024-03-30 11:15:51 +0100

    (main) : correction bugs historique

--- mern/client/src/Historique.js
+++ mern/client/src/Historique.js
@@ -4,7 +4,7 @@ import './Historique.css';
 
 function Historique() {
   const [label, setLabel] = useState([
-    "Nom", "sexe", "animal de compagnie", "MBTI", "siècle", "nationalité", "domaine", "formation", "récompense"
+    "Nom", "Sexe", "Animal", "MBTI", "Siecle", "Nationalite", "Domaine", "Formation", "Recompense"
   ]);
   const [data, setData] = useState([]);
 
@@ -13,19 +13,19 @@ function Historique() {
       try {
         const response = await axios.get('/api/historique');
         const newData = response.data;
-
-        // Vérifier si les nouvelles données existent déjà dans le tableau
-        if (!data.some(row => JSON.stringify(row) === JSON.stringify(newData))) {
-          // Si les nouvelles données ne sont pas déjà présentes, les ajouter au tableau
-          setData(prevData => [...prevData, newData]);
-        }
+        setData(newData)
+        // // Vérifier si les nouvelles données existent déjà dans le tableau
+        // if (!data.some(row => JSON.stringify(row) === JSON.stringify(newData))) {
+        //   // Si les nouvelles données ne sont pas déjà présentes, les ajouter au tableau
+        //   setData(prevData => [...prevData, newData]);
+        // }
       } catch (error) {
         console.error('Erreur lors de la récupération des données depuis l\'API:', error);
       }
     };
 
     // Rafraîchir les données toutes les 5 secondes
-    const intervalId = setInterval(fetchData, 5000);
+    const intervalId = setInterval(fetchData, 1000);
 
     // Nettoyer l'intervalle lorsque le composant est démonté
     return () => clearInterval(intervalId);
